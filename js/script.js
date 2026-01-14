e6ea1bddf0aa79c83027e36260d30b9e6855b955
NabinMahanty <nabinmahanty2003@gmail.com>
2026-01-14 10:35:31 +0530

    style: update CSS for body.active and improve HTML structure; enhance JavaScript syntax

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * add event on element
@@ -14,9 +12,7 @@ const addEventOnElem = function (elem, type, callback) {
   } else {
     elem.addEventListener(type, callback);
   }
-}
-
-
+};
 
 /**
  * navbar toggle
@@ -31,7 +27,7 @@ const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
   document.body.classList.toggle("active");
-}
+};
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
@@ -39,11 +35,9 @@ const closeNavbar = function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
   document.body.classList.remove("active");
-}
+};
 
 addEventOnElem(navbarLinks, "click", closeNavbar);
-
-
 
 /**
  * header active when window scroll down to 100px
@@ -57,6 +51,6 @@ const activeElemOnScroll = function () {
   } else {
     header.classList.remove("active");
   }
-}
+};
 
 addEventOnElem(window, "scroll", activeElemOnScroll);
